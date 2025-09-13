40cfcaa62418c4daf1ff62fe7b22e12dc77cd3ea
batmaans <gusttavor16@gmail.com>
2025-09-13 12:55:50 -0300

    primeiro envio

---
+++ script.js
@@ -0,0 +1,7 @@
+function selecionarImgProduto(imgMiniaturaEscolhida){
+    document.getElementById("imgPrincipal").src = imgMiniaturaEscolhida.src;
+}
+
+function mudarPreco(botaoEscolhido){
+    
+}
