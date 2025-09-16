1d23f89fa81ece1473d006171230a58ae26291e5
batmaans <gusttavor16@gmail.com>
2025-09-16 09:18:06 -0300

    envio 16 09 2

--- script.js
+++ script.js
@@ -2,6 +2,48 @@ function selecionarImgProduto(imgMiniaturaEscolhida){
     document.getElementById("imgPrincipal").src = imgMiniaturaEscolhida.src;
 }
 
-function mudarPreco(botaoEscolhido){
-    
+function mudarPreco(botaoEscolhido) {
+    const imagensModelos = {
+        "btnModeloPreto": "img/produto1.png",
+        "btnModeloAzul": "img/produto2.png",
+        "btnModeloVerde": "img/produto3.png",
+        "btnModeloCinza": "img/produto4.png",
+        "btnModeloRosa": "img/produto5.png"
+    };
+
+    // Troca a imagem principal
+    document.getElementById("imgPrincipal").src = imagensModelos[botaoEscolhido.id];
+
+    // Atualiza preço e quantidade conforme modelo
+    if(botaoEscolhido.id == "btnModeloPreto"){
+        document.getElementById("precoProduto").innerHTML = "R$ 56,90";
+        document.getElementById("qtdDisponivel").innerHTML = "171 peças disponíveis";
+    } else if(botaoEscolhido.id == "btnModeloAzul"){
+        document.getElementById("precoProduto").innerHTML = "R$ 56,90";
+        document.getElementById("qtdDisponivel").innerHTML = "152 peças disponíveis";
+    } else if(botaoEscolhido.id == "btnModeloVerde"){
+        document.getElementById("precoProduto").innerHTML = "R$ 49,90";
+        document.getElementById("qtdDisponivel").innerHTML = "213 peças disponíveis";
+    } else if(botaoEscolhido.id == "btnModeloCinza"){
+        document.getElementById("precoProduto").innerHTML = "R$ 69,90";
+        document.getElementById("qtdDisponivel").innerHTML = "353 peças disponíveis";
+    } else if(botaoEscolhido.id == "btnModeloRosa"){
+        document.getElementById("precoProduto").innerHTML = "R$ 69,90";
+        document.getElementById("qtdDisponivel").innerHTML = "319 peças disponíveis";
+    }
+}
+
+function tamanhoSelecionado(tamanhoEscolhido){
+    if(tamanhoEscolhido.id == "btnTamanhoP"){
+        document.getElementById("tamanhoSelecionado").innerHTML = "Tamanho Selecionado: P";
+    }
+    else if(tamanhoEscolhido.id == "btnTamanhoM"){
+        document.getElementById("tamanhoSelecionado").innerHTML = "Tamanho Selecionado: M";
+    }
+    else if(tamanhoEscolhido.id == "btnTamanhoG"){
+        document.getElementById("tamanhoSelecionado").innerHTML = "Tamanho Selecionado: G";
+    }
+    else if(tamanhoEscolhido.id == "btnTamanhoGG"){
+        document.getElementById("tamanhoSelecionado").innerHTML = "Tamanho Selecionado: GG";
+    }
 }
